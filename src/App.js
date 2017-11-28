82b2a62ec3c6379ce790c3723efd560ade92f245
Ryan Kaplan <ryan_kaplan@me.com>
2017-11-28 11:33:27 -0800

    Adds UserInput and UserOutput to project. #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
+import UserInput from './UserInput/UserInput'
+import UserOutput from './UserOutput/UserOutput'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <UserInput />
+        <UserOutput />
       </div>
     )
   }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserOutput = props => {
+  return (
+    <div>
+      <p>This is a placeholder</p>
+      <p>This is a second placeholder</p>
+    </div>
+  )
+}
+
+export default UserOutput
