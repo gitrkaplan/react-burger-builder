7fc1ccb72672b45c9ded740e9a23f10d6440387d
Ryan Kaplan <ryan_kaplan@me.com>
2017-12-21 23:02:34 -0800

    Creates Validation component that checks to see if text length is long enough. #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import Validation from './Validation/Validation'
 
 class App extends Component {
   state = {
@@ -19,6 +20,7 @@ class App extends Component {
           value={this.state.userInput}
         />
         <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
       </div>
     )
   }
