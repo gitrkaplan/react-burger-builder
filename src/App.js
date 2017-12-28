3e97e1a4c52e8044e43886b8954699a86ad5fb2f
Ryan Kaplan <ryan_kaplan@me.com>
2017-12-27 22:05:00 -0800

    Finishes setting up project files for myBurger project.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import React, { Component } from 'react'
-import './App.css'
-import Validation from './Validation/Validation'
-import Char from './Char/Char'
 
 class App extends Component {
-  state = {
-    userInput: ''
-  }
-
-  inputChangedHander = event => {
-    this.setState({ userInput: event.target.value })
-  }
-
-  deleteCharHandler = index => {
-    const text = this.state.userInput.split('')
-    text.splice(index, 1)
-    const updatedText = text.join('')
-    this.setState({ userInput: updatedText })
-  }
-
   render() {
-    const charList = this.state.userInput.split('').map((el, index) => {
-      return (
-        <Char
-          character={el}
-          key={index}
-          clicked={() => this.deleteCharHandler(index)}
-        />
-      )
-    })
     return (
       <div className="App">
-        <input
-          type="text"
-          onChange={this.inputChangedHander}
-          value={this.state.userInput}
-        />
-        <p>{this.state.userInput}</p>
-        <Validation inputLength={this.state.userInput.length} />
-        {charList}
+        <h1>This is my app</h1>
       </div>
     )
   }
