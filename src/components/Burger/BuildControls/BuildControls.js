1f1dc09907d83c6c698d1d35591d921029ca4d78
Ryan Kaplan <ryan_kaplan@me.com>
2017-12-29 15:15:52 -0800

    Adds logic for removing ingredients to burger and renders to page. #4

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -15,6 +15,7 @@ const buildControls = props => (
     {controls.map(ctrl => (
       <BuildControl
         added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
         key={ctrl.label}
         label={ctrl.label}
       />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,13 +35,27 @@ class BurgerBuilder extends Component {
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
   }
 
-  removeIngredientHandler = type => {}
+  removeIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = this.state.ingredients[type] - 1
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = updatedCount
+    const priceReduction = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice - priceReduction
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+  }
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+        />
       </Aux>
     )
   }
